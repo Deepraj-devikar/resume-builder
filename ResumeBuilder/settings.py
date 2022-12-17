7f7eec8c5c06ce6bd89875f439d5dcf791f28fa8
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-17 18:51:03 +0530

    ADDED: Resume app created

--- ResumeBuilder/settings.py
+++ ResumeBuilder/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Own app
+    'Resume',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,8 @@ WSGI_APPLICATION = 'ResumeBuilder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'ResumeBuilder',
     }
 }
 
